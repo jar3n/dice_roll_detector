e7f8664469d84f24b7e8fa14d1a1bd53e931962a
jar3n <jamreng@proton.me>
2026-07-13 13:39:55 -0400

    added softtimer and noise floor scripts

---
+++ pico/proj_test_code/test_dice_roll_capture.py
@@ -0,0 +1,5 @@
+# this is where the timer is incorperated
+# need to see how well the current prototype
+# detects when dice rolls initiate
+# and when they complete
+
--- pico/proj_test_code/test_dice_tray_sensors.py
+++ pico/proj_test_code/test_dice_tray_sensors.py
@@ -1,5 +1,18 @@
 # testing the sensitivity of the dice tray
-# with the combined sensors 
+# with the combined sensors
+
+# this test is to see how they behave together
+# without any laggy printing
+
+# behavior is as expected the collision
+# oscillates with the vibration
+# instead of bottoming out and being
+# closed the whole time the dice is on there
+# i think i want to try a mechanical keyboard switch
+# so the actuation force is less and the dice weight will
+# bottom out the switch
+
+
 
 from machine import Pin, ADC, PWM
 from time import sleep
@@ -44,4 +57,6 @@ while True:
         
         
     except KeyboardInterrupt:
+        red_light.duty_u16(0)
+        green_light.duty_u16(0)
         break                  
